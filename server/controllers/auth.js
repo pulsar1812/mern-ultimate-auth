2fb4b6daa0cac00b5df272567bd16e9d8785bb61
Patrick Lee <cluster000@gmail.com>
2020-04-27 23:36:02 +0800

    Add jwt and Sendgrid email for signup

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,3 +1,6 @@
+const jwt = require('jsonwebtoken');
+const sendgridMail = require('@sendgrid/mail');
+
 const User = require('../models/User');
 
 // @desc    Signing up user
@@ -29,4 +32,48 @@ const User = require('../models/User');
 // @desc    Signing up user
 // @route   POST /api/auth/signup
 // @access  Public
-exports.signup = async (req, res) => {};
+exports.signup = async (req, res) => {
+  const { name, email, password } = req.body;
+
+  try {
+    let user = await User.findOne({ email });
+
+    if (user) {
+      return res.status(400).json({ error: 'Email is taken.' });
+    }
+
+    sendgridMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+    const token = jwt.sign(
+      { name, email, password },
+      process.env.JWT_ACCOUNT_ACTIVATION,
+      { expiresIn: 360000 } // 100 hours for testing purpose
+    );
+
+    const emailData = {
+      from: process.env.EMAIL_FROM,
+      to: email,
+      subject: `Account activation link`,
+      html: `
+        <h1>Please use the following link to activate your account</h1>
+        <p>${process.env.CLIENT_URL}/auth/activate</p>
+        <hr />
+        <p>This email may contain sensitive information</p>
+        <p>${process.env.CLIENT_URL}</p>
+      `,
+    };
+
+    const sent = await sendgridMail.send(emailData);
+
+    if (!sent) {
+      return res.status(400).json({ message: 'Signup email not sent' });
+    }
+
+    res.json({
+      message: `Email has been sent to ${email}. Follow the instruction to activate your account.`,
+    });
+  } catch (err) {
+    console.log('Signup Error', err);
+    return res.status(400).json({ error: err });
+  }
+};
