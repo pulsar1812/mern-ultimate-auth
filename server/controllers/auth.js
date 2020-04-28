53933ecccda0fbcd62e8f27bf89ea7d1db5392bb
Patrick Lee <cluster000@gmail.com>
2020-04-28 23:38:16 +0800

    Add account activation

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -6,32 +6,6 @@ const User = require('../models/User');
 // @desc    Signing up user
 // @route   POST /api/auth/signup
 // @access  Public
-// exports.signup = async (req, res) => {
-//   const { name, email, password } = req.body;
-
-//   try {
-//     let user = await User.findOne({ email });
-
-//     if (user) {
-//       return res.status(400).json({ error: 'Email is taken.' });
-//     }
-
-//     let newUser = new User({ name, email, password });
-
-//     await newUser.save();
-
-//     res.json({
-//       message: 'Signup Success! Please Sign in.',
-//     });
-//   } catch (err) {
-//     console.log('Signup Error', err);
-//     return res.status(400).json({ error: err });
-//   }
-// };
-
-// @desc    Signing up user
-// @route   POST /api/auth/signup
-// @access  Public
 exports.signup = async (req, res) => {
   const { name, email, password } = req.body;
 
@@ -56,7 +30,7 @@ exports.signup = async (req, res) => {
       subject: `Account activation link`,
       html: `
         <h1>Please use the following link to activate your account</h1>
-        <p>${process.env.CLIENT_URL}/auth/activate</p>
+        <p>${process.env.CLIENT_URL}/auth/activate/${token}</p>
         <hr />
         <p>This email may contain sensitive information</p>
         <p>${process.env.CLIENT_URL}</p>
@@ -66,7 +40,7 @@ exports.signup = async (req, res) => {
     const sent = await sendgridMail.send(emailData);
 
     if (!sent) {
-      return res.status(400).json({ message: 'Signup email not sent' });
+      return res.status(400).json({ message: 'Signup email sent error' });
     }
 
     res.json({
@@ -77,3 +51,40 @@ exports.signup = async (req, res) => {
     return res.status(400).json({ error: err });
   }
 };
+
+// @desc    Activate an account
+// @route   POST /api/auth/account-activation
+// @access  Public
+exports.accountActivation = async (req, res) => {
+  const { token } = req.body;
+
+  // Make sure token exists
+  if (!token) {
+    return res.status(401).json({ message: '' });
+  }
+
+  try {
+    // Verify token
+    const decoded = jwt.verify(token, process.env.JWT_ACCOUNT_ACTIVATION);
+
+    const { name, email, password } = decoded;
+
+    const user = new User({ name, email, password });
+
+    await user.save();
+
+    res.json({
+      message: 'Signup Success. Please Sign in.',
+    });
+  } catch (err) {
+    console.log('Account Activation Error', err);
+    res.status(401).json({
+      error: 'Error saving user in database. Try signup again.',
+    });
+  }
+};
+
+// @desc    Signing in user
+// @route   POST /api/auth/signin
+// @access  Public
+exports.signin = async (req, res) => {};
